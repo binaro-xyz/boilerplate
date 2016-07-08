8c2c72d8f4229e57c3d1fe621bf79e3910326494
Benjamin Altpeter <benjamin@oldpeter.de>
2016-07-08 14:13:42 +0200

    Added small utility function for menus

--- lib/boilerplate/Utility/Utility.php
+++ lib/boilerplate/Utility/Utility.php
@@ -22,4 +22,12 @@ class Utility
     public static function printRPre($expression) {
         echo '<pre>' . print_r($expression, true) . '</pre>';
     }
+
+    // use for navigation menus: determine if a certain page is active and add a class accordingly
+    public static function activePageClass(string $filename) : string {
+        if(strpos(basename($_SERVER['PHP_SELF']), $filename) !== false) {
+            return 'class="active"';
+        }
+        return '';
+    }
 }
