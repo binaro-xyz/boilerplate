d57fad21c2885bf4cc0d69678da04c16dca99215
Benjamin Altpeter <benjamin@oldpeter.de>
2016-07-18 21:31:02 +0200

    Moved Utility/Utility class to helpers.php file

---
+++ lib/boilerplate/helpers.php
@@ -0,0 +1,28 @@
+<?php
+
+function varDumpPre($expression) {
+    echo '<pre>';
+    var_dump($expression);
+    echo '</pre>';
+}
+
+// see https://secure.php.net/manual/en/function.var-dump.php#51119
+function varDumpReturn($expression) : string {
+    ob_start();
+    var_dump($expression);
+    $return = ob_get_contents();
+    ob_end_clean();
+    return $return;
+}
+
+function printRPre($expression) {
+    echo '<pre>' . print_r($expression, true) . '</pre>';
+}
+
+// use for navigation menus: determine if a certain page is active and add a class accordingly
+function activePageClass(string $filename) : string {
+    if(strpos(basename($_SERVER['PHP_SELF']), $filename) !== false) {
+        return 'class="active"';
+    }
+    return '';
+}
